f5a3d4b2d6c074f05a20362892f7ca9bc6fd3183
Ken Gilmer <kgilmer@gmail.com>
2012-05-28 22:33:52 -0400

    rrp: add view for station category.

--- workspace/com.abk.rrp/src/com/abk/rrp/PlayerActivity.java
+++ workspace/com.abk.rrp/src/com/abk/rrp/PlayerActivity.java
@@ -59,7 +59,7 @@ import com.abk.rrp.model.StreamDirectoryClient;
  * Entry point to application.
  * 
  * @author kgilmer
- *
+ * 
  */
 public class PlayerActivity extends GDActivity {
 	private final static String TAG = PlayerActivity.class.getPackage().getName();
@@ -83,71 +83,75 @@ public class PlayerActivity extends GDActivity {
 		setActionBarContentView(R.layout.paged_view);
 		addActionBarItem(Type.Settings, R.id.action_bar_settings);
 		addActionBarItem(Type.Refresh, R.id.action_bar_settings);
-		
+
 		final PagedView pagedView = (PagedView) findViewById(R.id.paged_view);
 		pagedView.setOnPageChangeListener(mOnPagedViewChangedListener);
-		
+
 		try {
 			pageIndicator = (PageIndicator) findViewById(R.id.page_indicator_other);
 
 			streamClient = new StreamDirectoryClient(API_KEY, getSharedPreferences(PREF_ROOT_NAME, MODE_PRIVATE));
-			
-			//TODO: need to call fillCache in a background thread and show modal "loading..." dialog on first app load.
+
+			// TODO: need to call fillCache in a background thread and show
+			// modal "loading..." dialog on first app load.
 			new LoadCategoriesTask().execute(streamClient);
-			//TODO: need to add to action bar: "Reload station data" which should delete pref data and fillcache().
+			// TODO: need to add to action bar: "Reload station data" which
+			// should delete pref data and fillcache().
 
 			primaryCategories = streamClient.getDirectories().get(0).getPrimaryCategories();
 			pagedView.setAdapter(new CategorySwipeAdapter(primaryCategories));
 			pageIndicator.setDotCount(primaryCategories.size());
 
-			setActivePage(pagedView.getCurrentPage());			
-			
+			setActivePage(pagedView.getCurrentPage());
+
 			mediaPlayer = new MediaPlayer();
 			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 		} catch (Exception e) {
 			Log.e(TAG, "An error occurred while initializing the player.", e);
 			showDialog("Error", "An error occurred while initializing the player.", "Exit", new OnClickListener() {
-				
+
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					//PlayerActivity.this.finish();
+					// TODO: enable following line for release.
+					// PlayerActivity.this.finish();
 				}
 			});
-		}				
+		}
 	}
-	
-	@Override
-    public boolean onHandleActionBarItemClick(ActionBarItem item, int position) {
 
-        switch (item.getItemId()) {
-            case R.id.action_bar_settings:
-            	System.out.println("settings");
-            	return true;
-            case R.id.action_bar_refresh:
-            	System.out.println("refresh");
-            	return true;
-        }
-        
-        return false;
+	@Override
+	public boolean onHandleActionBarItemClick(ActionBarItem item, int position) {
+
+		switch (item.getItemId()) {
+		case R.id.action_bar_settings:
+			System.out.println("settings");
+			return true;
+		case R.id.action_bar_refresh:
+			System.out.println("refresh");
+			return true;
+		}
+
+		return false;
 	}
 
 	private void setActivePage(int page) {
 		pageIndicator.setActiveDot(page);
+
 	}
 
 	synchronized private void playStream(String streamUrl, ImageView image) throws IllegalArgumentException, IllegalStateException, IOException {
 		if (mediaPlayerPlaying) {
 			stopStream();
 		}
-		
+
 		mediaPlayer.setDataSource(streamUrl);
-		mediaPlayer.prepare(); 
+		mediaPlayer.prepare();
 		mediaPlayer.start();
 		mediaPlayerPlaying = true;
-		
+
 		image.setImageResource(R.drawable.ic_stop);
 	}
-	
+
 	private void stopStream() {
 		mediaPlayer.stop();
 		mediaPlayer.reset();
@@ -155,10 +159,7 @@ public class PlayerActivity extends GDActivity {
 	}
 
 	private void showDialog(String title, String message, CharSequence buttonLabel, DialogInterface.OnClickListener clickListener) {
-		new AlertDialog.Builder(PlayerActivity.this)
-			.setTitle(title)
-			.setMessage(message)		
-			.setPositiveButton(buttonLabel, clickListener).show();
+		new AlertDialog.Builder(PlayerActivity.this).setTitle(title).setMessage(message).setPositiveButton(buttonLabel, clickListener).show();
 	}
 
 	private final OnPagedViewChangeListener mOnPagedViewChangedListener = new OnPagedViewChangeListener() {
@@ -203,7 +204,10 @@ public class PlayerActivity extends GDActivity {
 		}
 
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
+		public View getView(int position, View convertView, ViewGroup parent) {		
+			VerticalTextView categoryTextSwitcher = (VerticalTextView) findViewById(R.id.category_label);
+			categoryTextSwitcher.setText(primaryCategories.get(position).getName());
+
 			if (convertView == null) {
 				convertView = getLayoutInflater().inflate(R.layout.paged_view_item, parent, false);
 				((ListView) convertView).setOnItemClickListener(new StationSelectedListener());
@@ -223,7 +227,7 @@ public class PlayerActivity extends GDActivity {
 				} catch (JSONException e) {
 					Log.e(TAG, "The server returned invalid data.", e);
 					showDialog("Error", "The server returned invalid data.", "Exit", new OnClickListener() {
-						
+
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
 							PlayerActivity.this.finish();
@@ -232,7 +236,7 @@ public class PlayerActivity extends GDActivity {
 				} catch (IOException e) {
 					Log.e(TAG, "Unable to access station data from server.", e);
 					showDialog("Error", "Unable to access station data from server.", "Exit", new OnClickListener() {
-						
+
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
 							PlayerActivity.this.finish();
@@ -254,14 +258,14 @@ public class PlayerActivity extends GDActivity {
 			StreamDescription stream = (StreamDescription) parent.getItemAtPosition(position);
 
 			try {
-				ImageView image=(ImageView) view.findViewById(R.id.list_item_action_image);
+				ImageView image = (ImageView) view.findViewById(R.id.list_item_action_image);
 				image.setImageResource(R.drawable.ic_wait);
 				playStream(stream.getStreamUrl(), image);
 			} catch (IOException e) {
 				Log.e(TAG, "The server returned invalid data.", e);
-				//TODO: allow retry here.
+				// TODO: allow retry here.
 				showDialog("Error", "Unable to access station.", "Exit", new OnClickListener() {
-					
+
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						PlayerActivity.this.finish();
@@ -280,20 +284,20 @@ public class PlayerActivity extends GDActivity {
 			super(context, textViewResourceId, streamDescs);
 			this.streamDescs = streamDescs;
 		}
-		
+
 		@Override
 		public int getCount() {
-			
+
 			return streamDescs.size();
 		}
-		
+
 		@Override
 		public StreamDescription getItem(int position) {
 			return streamDescs.get(position);
 		}
-		
+
 		@Override
-		public long getItemId(int position) {		
+		public long getItemId(int position) {
 			return position;
 		}
 
@@ -304,24 +308,24 @@ public class PlayerActivity extends GDActivity {
 				vi = getLayoutInflater().inflate(R.layout.list_item, null);
 
 			TextView text = (TextView) vi.findViewById(R.id.list_item_title);
-			ImageView image=(ImageView)vi.findViewById(R.id.list_item_action_image);
+			ImageView image = (ImageView) vi.findViewById(R.id.list_item_action_image);
 			text.setText(streamDescs.get(position).getName());
 			image.setImageResource(R.drawable.ic_play);
 			// imageLoader.DisplayImage(data[position], image);
 			return vi;
 		}
 	}
-	
+
 	/**
 	 * @author kgilmer
-	 *
+	 * 
 	 */
 	private static class LoadCategoriesTask extends AsyncTask<StreamDirectoryClient, Integer, Void> {
 
 		@Override
 		protected Void doInBackground(StreamDirectoryClient... params) {
 			StreamDirectoryClient client = params[0];
-			
+
 			List<IStreamSource> directories = client.getDirectories();
 			int count = directories.size();
 			int index = 1;
@@ -334,11 +338,11 @@ public class PlayerActivity extends GDActivity {
 					index++;
 				} catch (Exception e) {
 					Log.e(TAG, "An error occurred while loading station data.", e);
-				} 		
+				}
 			}
-			
+
 			return null;
 		}
-		
+
 	}
 }
